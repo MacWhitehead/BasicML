572bdd158d6027b4faafc07b2a485daf837ceabd
Jaiden Fisher <jaiden.fisher@gmail.com>
2024-06-07 20:29:45 -0600

    Added equality & casting operators to Word.cs

--- BasicML/Word.cs
+++ BasicML/Word.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace BasicML
 {
@@ -22,6 +23,13 @@ namespace BasicML
 		public static Word operator *(Word a, Word b) { return new Word(a._rawValue * b._rawValue); }
 		public static Word operator /(Word a, Word b) { return new Word(a._rawValue / b._rawValue); }
 
+		public static bool operator ==(Word a, Word b) { return a._rawValue == b._rawValue; }
+		public static bool operator !=(Word a, Word b) { return a._rawValue == b._rawValue; }
+
+		public static bool operator ==(Word a, int i) { return a._rawValue == i; }
+		public static bool operator !=(Word a, int i) { return a._rawValue == i; }
+
+		public static implicit operator Word(int i) => new Word(i);
 
 		public int Instruction
 		{
